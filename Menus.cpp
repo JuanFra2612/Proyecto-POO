fe5feabd0ede7a3768ef2271fe823a7ef2db744b
JuanFra2612 <juanfraglezcont@gmail.com>
2023-11-20 09:57:06 -0600

    Imagenes Portada

--- Menus.cpp
+++ Menus.cpp
@@ -4,32 +4,35 @@
 #include "Menus.h"
 
 void Portada() {
-
+     setbkcolor(LIGHTGREEN);
+     cleardevice();
+     readimagefile("IPN.jpg",5,5,130,130);
+     readimagefile("ESIME.jpg",960,5,1095,130);
     // Dibujar el título
-    setcolor(WHITE);
+    setcolor(RED);
     settextstyle(4,0,4);
     outtextxy(250,50,"Instituto Politecnico Nacional");
 
     // Configurar colores y fuentes para la información
-    setcolor(YELLOW);
+    setcolor(RED);
     settextstyle(4,0,2);
     
     //Materia
-    setcolor(YELLOW);
+    setcolor(RED);
     outtextxy(100, 150, "Materia:");
-    setcolor(WHITE);
+    setcolor(BLACK);
     outtextxy(225, 150, "Programacion Orientada a Objetos");
 
     //Integrantes
-    setcolor(YELLOW);
+    setcolor(RED);
     outtextxy(100, 200, "Integrantes:");
-    setcolor(WHITE);
+    setcolor(BLACK);
     outtextxy(120, 250, "1. González Contreras Juan Francisco");
     outtextxy(120, 280, "2. Chino Lopez Ulises");
     outtextxy(120, 310, "3. Gilberto");
     
-    setcolor(YELLOW);
+    setcolor(RED);
     outtextxy(100, 400, "Tema:");
-    setcolor(WHITE);
+    setcolor(BLACK);
     outtextxy(200, 400, "Proyecto Final");
 }
